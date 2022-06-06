b44f682c49b629cd76dc87f36170633b6b3faf08
Edward Tanguay (windows) <edward@tanguay.info>
2022-06-06 15:17:02 +0200

    reading data

--- pages/tech-items/[id].js
+++ pages/tech-items/[id].js
@@ -1,18 +1,36 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useState } from 'react';
 import Head from 'next/head';
 import { SiteHeader } from '../../components/SiteHeader';
 import { Nav } from '../../components/Nav';
-import techItems from '../../data/techItems.json';
 import { NavLink } from '../../components/NavLink';
 import { useRouter } from 'next/router';
 import * as qstr from '../../qtools/qstr';
+import axios from 'axios';
+
+const url = 'https://edwardtanguay.netlify.app/share/techitems.json';
 
 const TechItems = () => {
+	const [techItem, setTechItem] = useState({});
 	const router = useRouter();
-	const { id } = router.query;
-	// const techItem = techItems.find((m) => String(m.id) === id);
-	// console.log(techItem);
-	// const description = qstr.getQuickDefinitionFromExtras(techItem.extras);	
-	const description = qstr.getQuickDefinitionFromExtras('nn');	
+
+	useEffect(() => {
+		if (!router.isReady) return;
+		const id = router.query['id'];
+		console.log('333', id);
+		(async () => {
+			const _techItems = (await axios.get(url)).data;
+			const _techItem = _techItems.find((m) => String(m.id) === id);
+			console.log('111', _techItems);
+			console.log('222', _techItem);
+			if (_techItem) {
+				_techItem.description = qstr.getQuickDefinitionFromExtras(
+					_techItem.extras
+				);
+				setTechItem(_techItem);
+			}
+		})();
+	}, [router.isReady]);
 
 	return (
 		<>
@@ -26,8 +44,13 @@ const TechItems = () => {
 			</Head>
 			<SiteHeader />
 			<Nav />
-			{/* {techItem && <h3>{techItem.title} - {description}</h3>} */}
-			{<h3>{description}</h3>}
+			<h3>
+				{Object.entries(techItem).length > 0 && (
+					<>
+						{techItem.title} - {techItem.description}
+					</>
+				)}
+			</h3>
 		</>
 	);
 };
