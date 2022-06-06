7dab927ed21eb17566e1a094b1947b30af079d71
Edward Tanguay (windows) <edward@tanguay.info>
2022-06-06 11:10:39 +0200

    seo test

--- components/Nav.js
+++ components/Nav.js
@@ -1,9 +1,15 @@
+import { NavLink } from './NavLink';
+
 export const Nav = () => {
     return (
         <nav>
-            <span>Welcome</span>
+            <span>
+                <NavLink exact href="/">Welcome</NavLink>
+            </span>
             <span className="separator">|</span>
-            <span>Code</span>
+            <span>
+                <NavLink href="/code">Code</NavLink>
+            </span>
         </nav>
     );
 };
--- pages/code.js
+++ pages/code.js
@@ -1,9 +1,18 @@
+import Head from 'next/head';
 import { Header } from '../components/Header';
 import { Nav } from '../components/Nav';
 
 const Code = () => {
 	return (
 		<>
+			<Head>
+				<title>Edward&apos;s Code Examples</title>
+				<meta
+					name="description"
+					content="Lots of code examples to help you in your development."
+				/>
+				<link rel="icon" href="/code.ico" />
+			</Head>
 			<Header />
 			<Nav />
 			<h2>Code Page</h2>
